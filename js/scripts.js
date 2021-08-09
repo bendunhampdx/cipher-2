1814602eb6e4ec8f1401fa8f80dbcee0aea248ee
bendunhampdx <bendunhampdx@gmail.com>
2021-08-09 16:14:43 -0700

    Get second function to work within first function

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,15 @@ alert(sentence);
 function letters() {
   let letter1 = sentence.toUpperCase().charAt(0);
   let letter2 = sentence.toUpperCase().charAt(sentence.length -1);
-  let newSentence = letter1 + letter2;
-  let newSentence2 = newSentence.split("").reverse().join("").toString();  
-  let newSentence3 = sentence + newSentence2;
-  let halfChar = newSentence3.charAt(newSentence3.length / 2).split("").toString("");
-  let response = halfChar + newSentence3;
-  let final = response.split("").reverse().join("").toString();
-  alert(final)
+  let combo = letter1 + letter2;
+  alert(combo)
+  function reverseLetters() {
+ 
+    let reverseCombo = combo.split("").reverse().join("").toString();
+    alert(reverseCombo);
+    console.log("Hello Benjamin")
+  }
+  reverseLetters();
 };
 letters();
 
-// function reverse() {
-//   console.log("made it here");
-//   let rMessage = newSentence.split(" ").reverse();
-//   return alert(rMessage);
-// }
-// reverse();
