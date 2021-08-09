8714f43efa557aca2a7df9999aea9377130cf5c5
bendunhampdx <bendunhampdx@gmail.com>
2021-08-09 15:34:38 -0700

    get all cipher commands working in one function

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,23 @@
 let sentence = prompt("Please enter a sentence.");
 alert(sentence);
 
-console.log("made it here");
+
 function letters() {
   let letter1 = sentence.toUpperCase().charAt(0);
   let letter2 = sentence.toUpperCase().charAt(sentence.length -1);
-  return alert(letter1 + letter2);
-
-
+  let newSentence = letter1 + letter2;
+  let newSentence2 = newSentence.split("").reverse().join("").toString();  
+  let newSentence3 = sentence + newSentence2;
+  let halfChar = newSentence3.charAt(newSentence3.length / 2).split("").toString("");
+  let response = halfChar + newSentence3;
+  let final = response.split("").reverse().join("").toString();
+  alert(final)
 };
 letters();
+
+// function reverse() {
+//   console.log("made it here");
+//   let rMessage = newSentence.split(" ").reverse();
+//   return alert(rMessage);
+// }
+// reverse();
